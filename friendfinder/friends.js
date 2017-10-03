ca9f885c516d7574df260577fe3421dc19b66c0d
karimechimal <karime.chimal@gmail.com>
2017-10-03 12:39:19 -0500

    Add files via upload

---
+++ friendfinder/friends.js
@@ -0,0 +1,9 @@
+var dataArray= [
+	{
+		name: "Karime Chimal",
+		photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwjDo_eZ_dTWAhUBxmMKHaByAu0QjBwIBA&url=http%3A%2F%2Fscontent.cdninstagram.com%2Ft51.2885-15%2Fs480x480%2Fe15%2Fc0.89.720.720%2F16789844_1816954851900965_2647965174706733056_n.jpg%3Fig_cache_key%3DMTQ1NjkwMTk4NjgxMjQ1OTEzMw%253D%253D.2.c&psig=AOvVaw1ZbIggoNoPwKFJN_f88Ezx&ust=1507138128104267",
+		scores: ["5","5","5","5","5","5","5","5","5","5"]
+	}
+]
+
+module.exports = dataArray
